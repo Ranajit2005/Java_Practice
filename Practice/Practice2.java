9a1af9f05695cbd26ebba45d850769211bb5dfcc
Ranajit2005 <18ranajit2005@gmail.com>
2025-01-16 20:09:11 +0530

    Practice1

---
+++ Practice/Practice2.java
@@ -0,0 +1,9 @@
+//package Practice;
+
+//Create a class EMP having instance variable name and id. Create its subclass (say Scientist) which has instance variable no_of_publication and experience. Now create its subclass, say Dscientist which has instance variable award. Put a method: public String toString () { } in every class where you describe about the class and from main create object of each class and print each object.
+
+public class Practice2 {
+    public static void main(String[] args) {
+        
+    }
+}
