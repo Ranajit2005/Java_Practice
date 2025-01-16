4cd84ea8dea45d3ba3b2b9d2272b6db8685fab7e
Ranajit2005 <18ranajit2005@gmail.com>
2025-01-16 20:45:14 +0530

    Practice2

--- Practice/Practice2.java
+++ Practice/Practice2.java
@@ -1,10 +1,15 @@
 //package Practice;
 
-//Create a class EMP having instance variable name and id. Create its subclass (say Scientist) which has instance variable no_of_publication and experience. Now create its subclass, say Dscientist which has instance variable award. Put a method: public String toString () { } in every class where you describe about the class and from main create object of each class and print each object.
+//Create a class EMP having instance variable name and id. Create its subclass (say Scientist) which has instance variable no_of_publication and experience. Now create its subclass, say Dscientist which has instance variable award. Put a method: public String toString () { } in every class where you describe about the class and from main create object of each class and print each object. Do the problem 4 using dynamic method dispatching
 
 class EMP{
     String name;
     int id;
+
+    EMP(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
 
     public String toString(){
         return "Name: " + name + "ID : " + id;
@@ -16,6 +21,12 @@ class Scientist extends EMP{
     int no_of_publication;
     int experience;
 
+    Scientist(int a,int b){
+        super("Scientist",2);
+        this.no_of_publication = a;
+        this.experience = b;
+    }
+
     public String toString(){
         return "Name: "+name+" ID: "+id+" No of publication: "+no_of_publication+" Experience: "+experience;
     }
@@ -24,6 +35,11 @@ class Scientist extends EMP{
 
 class Dscientist extends Scientist{
     String award;
+
+    Dscientist(String award,int no_of_publication,int experience){
+        super(no_of_publication,experience);
+        this.award = award;
+    }
 
     public String toString(){
         return "Name: "+name+" ID: "+id+" No of publication: "+no_of_publication+" Experience: "+experience+" Award: "+award;
@@ -34,24 +50,14 @@ class Dscientist extends Scientist{
 public class Practice2 {
     public static void main(String[] args) {
 
-        EMP e = new EMP();
-        e.name = "EMP";
-        e.id = 1;
+        EMP e = new EMP("EMP",1);
         System.out.println(e);
 
-        Scientist s = new Scientist();
-        s.name = "Scientist";
-        s.id = 2;
-        s.no_of_publication = 3;
-        s.experience = 4;
+        Scientist s = new Scientist(2,3);
         System.out.println(s);
 
-        Dscientist ds = new Dscientist();
-        ds.name = "Dscientist";
-        ds.id = 3;
-        ds.no_of_publication = 5;
-        ds.experience = 6;
-        ds.award = "Best Scientist";
+        Dscientist ds = new Dscientist("abc",4,5);
+        
         System.out.println(ds);
         
     }
