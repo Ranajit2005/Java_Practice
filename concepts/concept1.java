e83c316a2653b47ccf4a1b82c1b9972a1fe0cc71
Ranajit2005 <18ranajit2005@gmail.com>
2026-04-26 14:21:24 +0530

    concept 1

---
+++ concepts/concept1.java
@@ -0,0 +1,35 @@
+class A{
+    void fun1(){
+        System.out.println("Hello from A fun 1 non static function(instance member function)");
+    }
+
+    void fun2(){
+        System.out.println("Hello from A fun 2 non static function(instance member function)");
+    }
+}
+
+class B{
+    static void fun1(){
+        System.out.println("Hello from B fun 1 static function");
+    }
+
+    static void fun2(){
+        System.out.println("Hello from B fun 2 static function");
+    }
+}
+
+class concept1{
+    public static void main(String[] args) {
+
+        // A.fun1();   // we can not call non static function without creating object of class
+        // A.fun2();
+
+        B.fun1();   // we can call static function without creating object of class
+        B.fun2();
+
+        A a = new A();
+        a.fun1();   // we can not call non static function without creating object of class
+        a.fun2();
+
+    }
+}
